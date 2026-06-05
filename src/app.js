f6ebc5ddb81f58a4662cae9e6d8c50e8ce6ebde6
Grzegorz Wozniak <gwozniak@mail.com>
2026-06-05 19:14:03 +0100

    obsluga formularza kontenera

--- src/app.js
+++ src/app.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
 
-   $("#form-tlumaczenie").submit(function(event) {
+   // Ukryć tłumaczenie jeśli nie jest jeszcze dostępne
+   $('#wynik-kontener').hide()
+
+   function tlumaczRzeczownik(event) {
       event.preventDefault()
-      console.log(event.target.rzeczownik.value)
-   })
+      const rzeczownik = event.target.rzeczownik.value
+
+      // Wysylanie zapytania do serwera
+      // Otrzymujemy tlumaczenie
+
+      // Wyswietlamy je na stronie
+      // Symulujemy odpowiedz serwera
+      const rezultat = {
+         rzeczownik: rzeczownik,
+         tlumaczenie: 'chien',
+         rodzajnik: 'le',
+         tag: 'rodzaj męski (masculin)'
+      }
+
+      // Wyświetlić cały kontener z tłumaczeniem
+      $('#wynik-kontener').show()
+
+      // Przypisz rzeczownik do "#do-tlumaczenia"
+      $('#do-tlumaczenia').text(rezultat.rzeczownik)
+
+      // Przypisz tlumaczenie do "#wynik"
+      $('#wynik').text(rezultat.tlumaczenie)
+
+      // Przypisz rodzajnik do "#rodzajnik"
+      $('#rodzajnik').text(rezultat.rodzajnik)
+
+      // Przypisz tag do "#tag"
+      $('#tag').text(rezultat.tag)
+
+   }
+
+   $("#form-tlumaczenie").submit(tlumaczRzeczownik)
 
 })

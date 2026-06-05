79aecf6fc6a2197e95269e708fdb23031124b059
Grzegorz Wozniak <gwozniak@mail.com>
2026-06-05 19:27:37 +0100

    symulacja slownika

--- src/app.js
+++ src/app.js
@@ -1,10 +1,61 @@
+function tlumacz(rzeczownik) {
+   const slownik = [
+      {
+         rzeczownik: 'pies',
+         tlumaczenie: 'chien',
+         rodzajnik: 'le',
+         tag: 'rodzaj męski (masculin)'
+      },
+      {
+         rzeczownik: 'kot',
+         tlumaczenie: 'chat',
+         rodzajnik: 'le',
+         tag: 'rodzaj męski (masculin)'
+      },
+      {
+         rzeczownik: 'dom',
+         tlumaczenie: 'maison',
+         rodzajnik: 'la',
+         tag: 'rodzaj żeński (féminin)'
+      },
+      {
+         rzeczownik: 'samochód',
+         tlumaczenie: 'voiture',
+         rodzajnik: 'la',
+         tag: 'rodzaj żeński (féminin)'
+      },
+      {
+         rzeczownik: 'drzewo',
+         tlumaczenie: 'arbre',
+         rodzajnik: 'le',
+         tag: 'rodzaj męski (masculin)'
+      }
+   ]
+
+   return slownik.find(wpis => wpis.rzeczownik === rzeczownik)
+}
+
+const rzeczowniki = [
+   'pies',
+   'kot',
+   'dom',
+   'samochód',
+   'drzewo'
+]
+
 $(document).ready(function() {
 
    // Ukryć tłumaczenie jeśli nie jest jeszcze dostępne
    $('#wynik-kontener').hide()
 
+   // Dodać rzeczowniki do selecta
+   rzeczowniki.forEach(slowo => {
+      $('#rzeczownik').append(new Option(slowo, slowo))
+   })
+
    function tlumaczRzeczownik(event) {
       event.preventDefault()
+      // Wybrana opcja z selecta
       const rzeczownik = event.target.rzeczownik.value
 
       // Wysylanie zapytania do serwera
@@ -12,12 +63,7 @@ $(document).ready(function() {
 
       // Wyswietlamy je na stronie
       // Symulujemy odpowiedz serwera
-      const rezultat = {
-         rzeczownik: rzeczownik,
-         tlumaczenie: 'chien',
-         rodzajnik: 'le',
-         tag: 'rodzaj męski (masculin)'
-      }
+      const rezultat = tlumacz(rzeczownik)
 
       // Wyświetlić cały kontener z tłumaczeniem
       $('#wynik-kontener').show()
